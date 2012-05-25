8d297d84a9e127ac6499eb5d8eaedf127e0b9c26
Jeremy Bush <contractfrombelow@gmail.com>
2012-05-25 08:43:13 -0500

    Add error messaging to user create fields

--- application/classes/view/welcome/index.php
+++ application/classes/view/welcome/index.php
@@ -2,12 +2,32 @@
 
 class View_Welcome_Index extends KOstache
 {
+	public $errors = array();
+
+	public function has_errors()
+	{
+		return (bool) $this->errors();
+	}
+
+	public function errors()
+	{
+		$errors = array();
+		foreach ($this->errors as $key => $error)
+		{
+			$errors[] = array('key' => $key, 'error' => $error);
+		}
+
+		return $errors;
+	}
+
 	public function email()
 	{
 		return array(
 			'name' => 'email',
 			'id' => 'email',
 			'label' => 'Email',
+			'has_error' => (bool) arr::get($this->errors, 'email'),
+			'error' => arr::get($this->errors, 'email'),
 		);
 	}
 
@@ -17,6 +37,8 @@ class View_Welcome_Index extends KOstache
 			'name' => 'password',
 			'id' => 'password',
 			'label' => 'Password',
+			'has_error' => (bool) arr::get($this->errors, 'password'),
+			'error' => arr::get($this->errors, 'password'),
 		);
 	}
 }
--- spec/view/WelcomeIndexSpec.php
+++ spec/view/WelcomeIndexSpec.php
@@ -22,4 +22,41 @@ class DescribeWelcomeIndex extends \PHPSpec\Context
 		$this->spec($field['id'])->should->equal('password');
 		$this->spec($field['label'])->should->equal('Password');
 	}
+
+	public function itAcceptsAnErrorArray()
+	{
+		$errors = array('email' => 'Email error', 'password' => 'Password error');
+		$this->subject->errors = $errors;
+		$this->spec($this->subject->errors())->should->equal(
+			array(
+				array('key' => 'email', 'error' => 'Email error'),
+				array('key' => 'password', 'error' => 'Password error'),
+			)
+		);
+	}
+
+	public function itKnowsWhenThereIsAnError()
+	{
+		$errors = array('email' => 'Email error', 'password' => 'Password error');
+		$this->subject->errors = $errors;
+		$this->spec($this->subject->has_errors())->should->beTrue();
+	}
+
+	public function itHighlightsTheEmailFieldForEmailError()
+	{
+		$errors = array('email' => 'Email error');
+		$this->subject->errors = $errors;
+		$email_field = $this->subject->email();
+		$this->spec($email_field['has_error'])->should->beTrue();
+		$this->spec($email_field['error'])->should->equal('Email error');
+	}
+
+	public function itHighlightsThePasswordFieldForPasswordError()
+	{
+		$errors = array('password' => 'Password error');
+		$this->subject->errors = $errors;
+		$password_field = $this->subject->password();
+		$this->spec($password_field['has_error'])->should->beTrue();
+		$this->spec($password_field['error'])->should->equal('Password error');
+	}
 }
